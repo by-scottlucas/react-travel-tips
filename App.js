cae1a7c8b0c45015266d81e1b173fca6a1096cb3
Lucas S <Lucasluke307@gmail.com>
2024-01-31 23:40:11 -0300

    Atualizando handleGenerate

--- App.js
+++ App.js
@@ -15,48 +15,51 @@ export default function App() {
   const [travel, setTravel] = useState("");
 
   async function handleGenerate() {
-    if (city === "") {
-      Alert.alert("Atenção", "Preencha o nome da cidade!");
-      return;
+
+    try {
+
+      if (city === "") {
+        Alert.alert("Atenção", "Preencha o nome da cidade!");
+        return;
+      }
+
+      setTravel("");
+      setLoading(true);
+      Keyboard.dismiss();
+
+      const prompt = `Crie um roteiro para uma viagem de exatos ${days.toFixed(0)} dias na cidade de ${city}, busque por lugares turisticos, lugares mais visitados, seja preciso nos dias de estadia fornecidos e limite o roteiro apenas na cidade fornecida. Forneça apenas em tópicos com nome do local onde ir em cada dia.`;
+
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${KEY_GPT}`
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: 'user',
+              content: prompt
+            }
+          ],
+          temperature: 0.20,
+          max_tokens: 500,
+          top_p: 1,
+        })
+      });
+
+      const data = await response.json();
+      console.log(data.choices[0].message.content);
+      setTravel(data.choices[0].message.content);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-
-    setTravel("");
-    setLoading(true);
-    Keyboard.dismiss();
-
-    const prompt = `Crie um roteiro para uma viagem de exatos ${days.toFixed(0)} dias na cidade de ${city}, busque por lugares turisticos, lugares mais visitados, seja preciso nos dias de estadia fornecidos e limite o roteiro apenas na cidade fornecida. Forneça apenas em tópicos com nome do local onde ir em cada dia.`
-
-    fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${KEY_GPT}`
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        message: [
-          {
-            role: 'user',
-            content: prompt
-          }
-        ],
-        temperature: 0.20,
-        max_tokens: 500,
-        top_p: 1,
-      })
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data.choices[0].message.content);
-        setTravel(data.choices[0].message.content)
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      })
   }
+
 
   return (
     <View style={styles.container}>
